c7504c380a2112a2f6db08f43d969ba9516e77cd
Milla Fyrman <143425911+MiFyr@users.noreply.github.com>
2024-05-19 00:59:07 +0200

    Add LoginForm
    Added a form for login and authorization + API-connection.

--- recipe-app-frontend/src/components/AuthComponents/LoginForm.jsx
+++ recipe-app-frontend/src/components/AuthComponents/LoginForm.jsx
@@ -3,12 +3,22 @@
 import "@/styles/forms.css";
 import { useState } from "react";
 import { handleFormChange } from "@/utils/forms";
+import { setAuthorization } from "@/utils/auth";
 
 export default function LoginForm() {
   const [formData, setFormData] = useState({ email: "", password: "" });
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+
+    try {
+      const response = await login(formData.email, formData.password);
+      setAuthorization(response);
+      location.href = "/";
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
 
     console.log(formData);
   };
@@ -18,7 +28,7 @@ export default function LoginForm() {
       onChange={handleFormChange(formData, setFormData)}
       onSubmit={handleFormSubmit}
     >
-      <h2>Login</h2>
+      <h2>Logga in</h2>
 
       <p className="error">{errorMessage}</p>
 
